ca0fa7d274f423d504284d48b05eddc37f040a3f
whoisgina <gina@rumo.rs>
2018-10-11 16:05:21 -0700

    Move plant card into component

---
+++ src/components/PlantCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="plant-card" class="plant-card">
+    <div class="plant-card__name">
+      {{ item.fields.Name }}
+    </div>
+
+    <div v-for="(photo, index) in item.fields.Photo"
+         :key="index">
+      <img class="plant-card__image" :src="photo.url" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PlantCard',
+  props: ['item']
+}
+</script>
+
+<style lang="scss">
+.plant-card {
+  background: #e0e0e0;
+  width: 20%;
+  margin: 1rem;
+  padding: 1rem;
+  flex-wrap: wrap;
+
+  &__name {
+    font-weight: bold;
+  }
+
+  &__image {
+    width: 100%;
+    width: 200px;
+    height: 200px;
+    object-fit: cover;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,55 +1,58 @@
 <template>
   <div class="home">
-    <div class="plant-card" v-for="(item, index) in items"
-         :key="index">
-      {{ item.fields.Name }}
-      <div v-for="(photo, index) in item.fields.Photo"
-           :key="index">
-        <img class="plant-card__image" :src="photo.url" />
-      </div>
-      
-    </div>
+    <plant-card v-for="(item, index) in items" 
+                :item="item" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import PlantCard from '@/components/PlantCard.vue'
 import axios from 'axios'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    PlantCard
   },
+
   data () {
     return {
       items: []
     }
   },
-  mounted () {
-    this.loadItems(); 
-  },
-  methods: {
-    loadItems: function(){
-        
-        // Init variables
-        var self = this
-        var app_id = "app0mdITu5g9AvhRY";
-        var app_key = "keydSWGobTOzrvf1e";
-        this.items = []
 
-    axios.get(
-        "https://api.airtable.com/v0/"+app_id+"/Studio%20plants",
+  mounted () {
+    this.loadItems()
+  },
+
+  methods: {
+    loadItems () {
+      var self = this
+      var appId = 'app0mdITu5g9AvhRY'
+      var appKey = 'keydSWGobTOzrvf1e'
+      this.items = []
+
+      axios.get(
+        'https://api.airtable.com/v0/' + appId + '/Studio%20plants',
         { 
-            headers: { Authorization: "Bearer "+app_key } 
+          headers: { Authorization: 'Bearer ' + appKey }
         }
-        ).then(function(response){
-            self.items = response.data.records
-        }).catch(function(error){
-            console.log(error)
-        })
+      ).then(function (response) {
+        self.items = response.data.records
+      }).catch(function (error) {
+        console.log(error)
+      })
     }
   }
 }
 </script>
+
+<style lang="scss">
+.home {
+  display: flex;
+  flex-wrap: wrap;
+  font-family: 'Styrene B';
+}
+</style>
