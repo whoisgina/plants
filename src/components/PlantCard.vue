06108829ec08ffa8b4d492cb1be523af718e715b
whoisgina <gina@rumo.rs>
2018-10-12 17:25:45 -0700

    Make plant cards Rumors-y

--- src/components/PlantCard.vue
+++ src/components/PlantCard.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="plant-card" class="plant-card">
-    <div class="plant-card__name">
-      {{ plant.fields.Name }}
-    </div>
-    <div class="plant-card__species">
-      {{ species.find(x => x.id === plant.fields.Species[0]).fields.Species }}
-    </div>
     <div v-for="(photo, index) in plant.fields.Photo"
          :key="index">
       <img class="plant-card__image" :src="photo.thumbnails.large.url" />
+    </div>
+
+    <div class="plant-card__info">
+
+      <div class="plant-card__name">
+        {{ plant.fields.Name }}
+      </div>
+      <div class="plant-card__species">
+        {{ species.find(x => x.id === plant.fields.Species[0]).fields.Species }}
+      </div>
     </div>
   </div>
 </template>
@@ -26,23 +30,31 @@ export default {
 <style lang="scss">
 
 .plant-card {
-  background: $color-gray-light;
-  flex-wrap: wrap;
-  margin: 1rem;
-  padding: 1rem;
-  width: 100%;
+  @include shadow;
+  border: 1px solid $color-black;
+  overflow: hidden;
+  position: relative;
+  max-width: 400px;
 
-  @include tablet { width: 40%; }
-  @include desktop { width: 20%; }
+  &__info {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    padding: 1rem;
+    background: $color-white;
+  }
 
   &__name {
     font-weight: bold;
   }
 
+  &__species { font-style: italic; }
+
   &__image {
-    height: 200px;
+    height: 400px;
     object-fit: cover;
-    width: 200px;
+    width: 400px;
   }
 }
 </style>
