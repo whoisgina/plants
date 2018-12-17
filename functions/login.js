0fcacbb8eeb4615a3d99fe396fe4ddf58164d9f8
Caleb Misclevitz <caleb@Calebs-MacBook-Pro.local>
2018-12-17 15:48:01 -0800

    Adds conditional response and query parameter to the login function

--- functions/login.js
+++ functions/login.js
@@ -1,8 +1,30 @@
-const { PASSWORD } = process.env;
+import querystring from "querystring"
 
 exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: PASSWORD
+  // Only allow POST
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" }
+  }
+
+  // When the method is POST, the name will no longer be in the event’s
+  // queryStringParameters – it’ll be in the event body encoded as a query string
+  const params = querystring.parse(event.body)
+  const submission = params.submission
+  const { PASSWORD } = process.env;
+
+  if (submission === PASSWORD) {
+    return {
+      statusCode: 200,
+      body: {
+        authenticated: true
+      }
+    }
+  } else {
+    return {
+      statusCode: 200,
+      body: {
+        authenticated: false
+      }
+    }
   }
 }
