e98186a11359442d6cf640e84efe70cf2e2907ab
Caleb Misclevitz <caleb@Calebs-MacBook-Pro.local>
2018-12-17 15:55:37 -0800

    Changes the login function to allow URL parameters

--- functions/login.js
+++ functions/login.js
@@ -1,15 +1,5 @@
-import querystring from "querystring"
-
 exports.handler = async (event, context) => {
-  // Only allow POST
-  if (event.httpMethod !== "POST") {
-    return { statusCode: 405, body: "Method Not Allowed" }
-  }
-
-  // When the method is POST, the name will no longer be in the event’s
-  // queryStringParameters – it’ll be in the event body encoded as a query string
-  const params = querystring.parse(event.body)
-  const submission = params.submission
+  const submission = queryStringParameters.submission
   const { PASSWORD } = process.env;
 
   if (submission === PASSWORD) {
