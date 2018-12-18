d8c559dc8eef4b54baeb0034502ccf8a267b61f2
Caleb Misclevitz <caleb@Calebs-MacBook-Pro.local>
2018-12-17 16:07:44 -0800

    Update the login function

--- functions/login.js
+++ functions/login.js
@@ -1,19 +1,22 @@
+const { PASSWORD } = process.env
+
 exports.handler = async (event, context) => {
   const submission = event.queryStringParameters.submission
-  const { PASSWORD } = process.env
 
   if (submission === PASSWORD) {
     return {
       statusCode: 200,
       body: {
-        authenticated: true
+        authenticated: true,
+        message: submission
       }
     }
   } else {
     return {
       statusCode: 200,
       body: {
-        authenticated: false
+        authenticated: false,
+        message: submission
       }
     }
   }
