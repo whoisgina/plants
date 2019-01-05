54f4b38bdb2ff1341d7f04440f29a68f274300ae
Caleb Misclevitz <caleb@Calebs-MacBook-Pro.local>
2019-01-04 23:18:04 -0800

    Adds an airtable proxy function using node-fetch

---
+++ src/lambda/airtable.js
@@ -0,0 +1,20 @@
+import fetch from "node-fetch";
+
+const API_ENDPOINT =
+  "https://api.airtable.com/v0/app0mdITu5g9AvhRY/Studio%20plants";
+
+exports.handler = async (event, context) => {
+  return fetch(API_ENDPOINT, {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + process.env.API_KEY
+    }
+  })
+    .then(response => response.json())
+    .then(data => ({
+      statusCode: 200,
+      body: data.records
+    }))
+    .catch(error => ({ statusCode: 422, body: String(error) }));
+};
