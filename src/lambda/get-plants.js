261edc38bd94493d77aeb7a4f961e71b5f0798d0
Caleb Misclevitz <caleb@Calebs-MacBook-Pro.local>
2019-01-08 09:51:51 -0800

    Removes env var dump added for debugging

--- src/lambda/get-plants.js
+++ src/lambda/get-plants.js
@@ -1,6 +1,6 @@
 var Airtable = require('airtable')
 var base = new Airtable({apiKey: process.env.API_KEY}).base(process.env.APP_ID)
-console.log(process.env.API_KEY + ' ' + process.env.APP_ID)
+
 exports.handler = function(event, context, callback) {
   
   var response = []
