2fd55aa22ef817db9f75c34a93b38691f351aeb0
whoisgina <gina@rumo.rs>
2019-01-11 12:49:13 -0800

    Remove vuex
    bye as well

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import dotenv from 'dotenv'
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store'
 
 Vue.config.productionTip = false
 
 dotenv.config()
 
 new Vue({
-  store,
   render: h => h(App)
 }).$mount('#app')
