3db642d630102c29d331e76a40a6897f786ceb25
calebmisclevitz <calebmisclevitz@gmail.com>
2019-01-11 12:25:33 -0800

    Updates hardcoded table names to match new Airtable schema (#52)
    * Adds a Vue plugin to interface with airtable
    
    * Provides an interface for making fetch requests to the Airtable API
    
    Initialize the plugin like this:
    
    Vue.use(airtablePlugin, {
      appId: '',
      appKey: ''
    })
    
    * Uses the Airtable plugin to fetch and update recs
    
    - Adds dotenv
    - Adds WIP netlify function
    
    * Adds netlify-lambda to simplify local development
    
    * Installs netlify-lambda
    * Reorganizes serverless functions
    * Adds a hello-world example
    * Rewrites the login function to avoid async
    
    * Removes overrides from the netlify.toml
    
    * Refactors GET request to use airtable plugin
    
    * Moves secrets to .env
    * Uses custom Airtable API plugin instead of a fetch request
    directly in the home or app component
    * Moves the main GET request up a level from home to app
    
    * Adds an airtable proxy function using node-fetch
    
    * Use require instead of import for node-fetch
    
    * Adds a lambda function that acts as an API proxy
    
    * Uses the official Airtable package to interface with the
    Airtable API via Node
    
    * Requests the plant records from Airtable using the credentials
    safely stored in Netlify environment variables
    
    * Formats the response to match the REST API response
    
    * Returns plant records as stringified JSON or an error
    
    * Fixes an incorrect statusCode
    
    * Adds a temporary call to the new API proxy for testing
    
    * Git ignores built lambda functions
    
    * Adds a console log to trigger a branch deploy on netlify
    
    * Update env variable names and dump env vars for debugging
    
    * Changes env var namespace to include VUE_APP prefix
    
    * Removes env var dump added for debugging
    
    * Reconfigures git ignore and removes compiled lambda functions from root
    
    * Adds compiled lambda directory back to gitignore
    
    * Abstract the API proxy to accept a table as a URL parameter
    
    * Makes it work
    
    * Removes API plugins and services replaced by proxy functions
    
    * Uncomments HTTP method error handling
    
    * Replaces a hardcoded date with an argument variable
    
    * Updates table names to match new Airtable schema

--- src/lambda/update-record.js
+++ src/lambda/update-record.js
@@ -10,7 +10,7 @@ exports.handler = function(event, context, callback) {
   const recordId = JSON.parse(event.body).recordId
   const fields = JSON.parse(event.body).fields
 
-  base('Studio plants').update(recordId, fields, function done(err, record) {
+  base('Plants').update(recordId, fields, function done(err, record) {
 
     if (err) { 
       console.error(err); return; 
